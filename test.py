d8856882e794c33131f63bcb1aeff276b8a97d76
Leviii99 <nyiro.levente@gmail.com>
2020-03-14 10:22:57 +0100

    First

---
+++ test.py
@@ -0,0 +1,19 @@
+#print("Hello")
+#for i in range(5, 10, 2):
+#    print(i)
+
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+print()
+
+i = 0
+while True:  
+    print(i)  
+    i = i + 1  
+    if(i > 5):  
+        break
+

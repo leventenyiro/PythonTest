645af2d9c435ddc581dd9fc4c119598848db1d2c
Leviii99 <nyiro.levente@gmail.com>
2020-03-14 14:36:35 +0100

    Tkinter - GUI

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import osztaly
+import osztaly as os
+import tkinter as tk
 
-o = osztaly.Osztaly(5)
+o = os.Osztaly(5)
 print(o.x)
 s = o.szorzas(6)
 print(str(s))
+
+root = tk.Tk()
+canvas = tk.Canvas(root, height=600, width=1000, bg="#00A1E4")
+canvas.pack()
+root.title("Python GUI")
+
+
+root.mainloop()

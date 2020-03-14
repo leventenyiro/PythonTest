5f73d835eea333fd27186a11cb2a47dab02c3c85
Leviii99 <nyiro.levente@gmail.com>
2020-03-14 10:29:14 +0100

    Comment

--- test.py
+++ test.py
@@ -10,10 +10,10 @@ while i < 5:
 
 print()
 
-i = 0
-while True:  
-    print(i)  
-    i = i + 1  
-    if(i > 5):  
-        break
+#i = 0
+#while True:  
+#    print(i)  
+#    i = i + 1  
+#    if(i > 5):  
+#        break
 
